4b6c2c2ed3eb81f25c7f6a573cbae74e1af4993a
iiihassanii <hassanabdelazim.max@gmail.com>
2024-06-14 13:59:31 +0300

    Connect to Mongo db

---
+++ index.js
@@ -0,0 +1,8 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Could not connect to MongoDB', err));
